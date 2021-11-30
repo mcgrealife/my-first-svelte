280d440e4a08b9ae0c1d4f7ee02d861c54f03080
Michael McGreal <mcgrealife@gmail.com>
2021-11-30 13:23:55 -0500

    first commit

---
+++ index.svelte
@@ -0,0 +1,51 @@
+<script src='svelte.js'>
+
+    let items = [
+        {
+        title: "Wow svelete is easy",
+        faq: "It makes the most sense to my brain",
+        img: "https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/apple/285/keycap-digit-zero_30-fe0f-20e3.png",
+    },
+    {
+        title: "It reminds me of Swift, my favorite so far",
+        faq: "the binding syntax is intuitive",
+        img: "https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/apple/285/keycap-digit-one_31-fe0f-20e3.png",
+    },
+    {
+        title: "Beginner friendly",
+        faq: "It feels like HTML/CSS/JS, but with the super power of referencing {variables}",
+        img: "https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/apple/285/keycap-digit-two_32-fe0f-20e3.png",
+    }
+    ];
+</script>
+
+
+
+<h2>Svelete Test</h2>
+
+
+{#each items as i}
+    <details>
+        <summary> {i.title}</summary>
+            <p>{i.faq}</p>
+            <img src={i.img} alt="placeholder">
+    </details>
+{/each}
+
+
+<style>
+    h2 {
+        color: red;
+        font-family: Roboto;
+    }
+summary {
+    color: purple;
+    font-weight: Bold;
+    font-family: Open Sans;
+}
+p {
+    color: seagreen;
+    font-family: Arial, Helvetica, sans-serif;
+
+}
+</style>
